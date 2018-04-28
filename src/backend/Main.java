9e601da5463b1016a105c421ef91d496e4bfc5c6
Catherine Phillips <cphillips-19@peddie.org>
2018-04-28 09:38:26 -0400

    More test cases

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -2,31 +2,39 @@ package backend;
 
 public class Main {
 
-	static Board board;
-	static int[] secretPegs = {1, 2, 1, 1, 1};
-	static int[] guessOne = {2, 1, 1, 1, 1};
-	static int[] guessTwo = {2, 1, 3, 1, 1};
+	static Board boardOne;
+	static int[] secretPegsOne = {1, 2, 2, 2};
+	static int[] boardOneGuessOne = {3, 1, 1, 1};
+	static int[] boardOneGuessTwo = {2, 1, 3, 1};
+	static int[] boardOneGuessThree = {3, 3, 3, 3};
+	static int[] boardOneGuessFour = {2, 2, 2, 2};
+	
 
 	public static void main(String[] args) {
-		board = new Board(secretPegs, 5, 5, 5);
+		boardOne = new Board(secretPegsOne, 5, 4, 5);
 
-		board.addGuess(guessOne);
-		for(int i = 0; i < 5; i++) {
-			System.out.print(board.returnPegs()[i]);
-		}
-		System.out.println();
+		boardOne.addGuess(boardOneGuessOne);
+		printPegs(boardOne);
 		
-		board.addGuess(guessTwo);
-		for(int i = 0; i < 5; i++) {
-			System.out.print(board.returnPegs()[i]);
-		}
-		System.out.println();
+		boardOne.addGuess(boardOneGuessTwo);
+		printPegs(boardOne);
 		
-		board.addGuess(secretPegs);
-		for(int i = 0; i < 5; i++) {
-			System.out.print(board.returnPegs()[i]);
-		}
-		System.out.println();
+		boardOne.addGuess(boardOneGuessThree);
+		printPegs(boardOne);
+		
+		boardOne.addGuess(boardOneGuessFour);
+		printPegs(boardOne);
+		
+		boardOne.addGuess(secretPegsOne);
+		printPegs(boardOne);
 	}
 
+	public static void printPegs(Board board) {
+		for(int i = 0; i < 4; i++) {
+			System.out.print(board.returnPegs()[i]);
+		}
+		System.out.println();
+		
+	}
+	
 }
