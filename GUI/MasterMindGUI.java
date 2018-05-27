45eb82a70a792d8c48ad4fe9ffcff702653a0bfb
Winston Yang <wyang737@users.noreply.github.com>
2018-05-27 14:27:19 -0400

    Main Method

--- GUI/MasterMindGUI.java
+++ GUI/MasterMindGUI.java
@@ -1,216 +1,214 @@
-import java.awt.EventQueue;
-import java.awt.Font;
-import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.util.Random;
-public class MasterMindGUI {
+package backend;
 
-	private JFrame frame;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MasterMindGUI window = new MasterMindGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}); 
-	}
+public class Board {
 
-	/**
-	 * Create the application.
-	 */
-	public MasterMindGUI(){
-		initialize(); 
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	//initial sequence to set up the game
+	private int[] secretGuess;
+	private int numGuesses;
 	
-	private boolean debug = true;
-	private int counter = 0;
+	//current guess and the pegs from that guess
+	private int[] currentGuess;
+	private int[] currentPegs;
 	
-	private void initialize() {
-
-		frame = new JFrame();
-		frame.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		frame.getContentPane().setEnabled(false);
-		frame.setBounds(100, 100, 1474, 718);
-		frame.setTitle("MasterMind");
+	//number of rows and colors are adjustable 
+	private int numRows;
+	private int numColors;
+	
+	private ArrayList<int[]> possibleSolutions;
+	//whether you've won or not 
+	private boolean hasWon = false;
+	/**
+	 * creates a new board of mastermind
+	 * @param sequence the secret sequence to start with 
+	 * @param totalGuesses the total number of guesses before you lose the game 
+	 */
+	public Board(int[] sequence, int totalGuesses, int rows, int colors) {
+		secretGuess = sequence;
+		numRows = rows;
+		numColors = colors;
+		currentGuess = new int[numRows];
+		numGuesses = totalGuesses;
+		currentPegs = new int[numRows];
+		possibleSolutions = createCombos(rows, colors);
 		
-		ImageIcon BlackPeg = new ImageIcon("BLACK.gif");
-		MyIcon WhitePeg = new MyIcon();
-		ImageIcon NoPeg = new ImageIcon("NOCOLOR.gif");
-
-		String numRows = JOptionPane.showInputDialog("How many rows? (up to 6)");
-		String numColors = JOptionPane.showInputDialog("How many colors? (up to 6)");
-		int rows = Integer.parseInt(numRows);
-		int colors = Integer.parseInt(numColors);
-
-		Random rand = new Random();
-		int[] sequence = new int[rows];
-		if (debug){
-			System.out.print("sequence: ");
-		}
-		for (int i = 0; i < rows; i ++){
-			int num = rand.nextInt(colors);
-			sequence[i] = num;
-			if (debug){
-				System.out.print(num);
-			}
-		}
-
-		Board board = new Board(sequence, 10, rows, colors); 
-		MyComboBox[][] comboBoxes = new MyComboBox[10][rows];
-		MyComboBox[] winBoxes = new MyComboBox[rows];
-		JLabel[][] pegs = new JLabel[10][rows];
-
-		JLabel numGuesses = new JLabel("Number of possible guesses remaining: " + board.returnNumPossibleSolutions());
-		numGuesses.setBounds(906, 520, 513, 80);
-		numGuesses.setFont(new Font("Times New Roman", Font.PLAIN, 29));
-		frame.getContentPane().add(numGuesses);
-
-		JButton guessButton = new JButton("Guess");
-		guessButton.setBounds(938, 212, 132, 127);
-		guessButton.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		frame.getContentPane().add(guessButton);
-		guessButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int[] currentGuess = new int[rows];
-				if (debug){
-					System.out.println("");
-					System.out.print("guess: ");
-				}
-				for (int i = 0; i < comboBoxes[counter].length; i ++){
-					int num = comboBoxes[counter][i].getSelectedIndex() - 1; // fix for colors 0-numColors
-					currentGuess[i] = num;
-					comboBoxes[counter][i].setEnabled(false);
-					if (counter < 9){
-						comboBoxes[counter + 1][i].setEnabled(true);
-					}
-					if (debug){
-						System.out.print(num + " ");
-					}
-				}
-				if (debug){
-					System.out.println("");
-				}
-				board.addGuess(currentGuess);
-				int[] currentPegs = board.returnPegs();
-				boolean win = true;
-				if (debug){
-					System.out.print("pegs: ");
-				}
-				for (int i = 0; i < currentPegs.length; i ++){ // if all 2, then win 
-					if (currentPegs[i] == 2){
-						pegs[counter][i].setIcon(BlackPeg);
-					} else {
-						win = false;
-						if (currentPegs[i] == 1){
-							pegs[counter][i].setIcon(WhitePeg);
-						}
-					}
-					if (debug){
-						System.out.print(currentPegs[i] + " ");
-					}
-				}
-				if (debug){
-					System.out.println("");
-				}
-				if (win){
-					for (int i = 0; i < comboBoxes.length; i ++){ // disable all boxes
-						for (int j = 0; j < rows; j ++){
-							comboBoxes[i][j].setEnabled(false);
-						}
-					}
-					guessButton.setEnabled(false);
-					for (int i = 0; i < rows; i ++){ // display the sequence
-						winBoxes[i].setVisible(true);
-						winBoxes[i].setEnabled(false);
-						winBoxes[i].setSelectedIndex(sequence[i] + 1);
-					}
-
-					Icon youWin = new ImageIcon("youwin.gif");
-					Icon fireworks = new ImageIcon("Fireworks.gif");
-					Icon confetti = new ImageIcon("confetti.gif");
-					
-					JLabel lblNewLabel_50 = new JLabel(youWin);
-					lblNewLabel_50.setBounds(938, 42, 300, 154);
-					frame.getContentPane().add(lblNewLabel_50);
-					
-					JLabel lblNewLabel_51 = new JLabel(fireworks);
-					lblNewLabel_51.setBounds(938, 355, 314, 119);
-					frame.getContentPane().add(lblNewLabel_51);
-
-					JLabel lblNewLabel_52 = new JLabel(confetti);
-					lblNewLabel_52.setBounds(1085, 201, 167, 158);
-					frame.getContentPane().add(lblNewLabel_52);
-
-					numGuesses.setText("Number of possible guesses remaining: 0");
-					JOptionPane.showMessageDialog(frame, "You won!");
-				} else {
-				numGuesses.setText("Number of possible guesses remaining: " + board.returnNumPossibleSolutions());
-				counter ++;
-				}
-			}
-		});
-		
-		ImageIcon[] icons = 
-		{
-			 new ImageIcon("NOCOLOR.gif"), // Fully transparent GIF
-	         new ImageIcon("RED.gif"),
-	         new ImageIcon("GREEN.gif"),
-	         new ImageIcon("BLUE.gif"),
-	         new ImageIcon("YELLOW.gif"),
-	         new ImageIcon("BLACK.gif"),
-	         new ImageIcon("WHITE.gif")
-		};
-		
-		ImageIcon[] iconColors = new ImageIcon[colors + 1]; // colors + 1 to include the default transparent one
-		for (int i = 0; i < iconColors.length; i ++){
-			iconColors[i] = icons[i];
-		}
-		
-		for (int i = 0; i < 10; i ++){ // make the boxes
-			for (int j = 0; j < rows; j ++){
-				MyComboBox combobox = new MyComboBox();
-				DefaultComboBoxModel model = new DefaultComboBoxModel(iconColors);
-				combobox.setModel(model);
-				comboBoxes[i][j] = combobox;
-				combobox.setBounds(48 + 85 * i, 114 + 85 * j, 71, 71);
-				combobox.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-				frame.getContentPane().add(combobox);
-				
-				JLabel peg = new JLabel(NoPeg);
-				pegs[i][j] = peg;
-				peg.setBounds(50 + 85 * i, 200 + 85 * (rows - 1) + 24 * j, 69, 20);
-				frame.getContentPane().add(peg);
-			}
-		}
-		
-		for (int i = 1; i < comboBoxes.length; i ++){ // disable all boxes except the first row
-			for (int j = 0; j < rows; j ++){
-				comboBoxes[i][j].setEnabled(false);
-			}
-		}
-
-		
-		for (int i = 0; i < rows; i ++){ // make win boxes
-			MyComboBox winBox = new MyComboBox();
-			winBoxes[i] = winBox;
-			winBox.setBounds(1267, 114 + 85 * i, 71, 71);
-			winBox.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-			frame.getContentPane().add(winBox);
-			winBox.setVisible(false);
-		}
-
 	}
+	
+	public int[] getSecretGuess() {
+		return secretGuess;
+	}
+	
+	/**
+	 * adds a new guess to the board from the player character 
+	 * @param guess an array of 4 ints that represent the colors guessed
+	 */
+	public void addGuess(int[] guess) {
+		if(numGuesses > 0) {
+			currentGuess = guess;
+			numGuesses--;
+		} else {
+			System.out.println("No more guesses!");
+		}
+		currentPegs = getPegs(secretGuess, currentGuess);
+	}
+	
+	/**
+	 * returns the black and white pegs 
+	 * white peg - 1 - means there is one of the right color but in the wrong place
+	 * black peg - 2 - means there is one with the right placement and color
+	 * @return an array of 4 ints that represent the small black and white pegs for the current guess
+	 */
+	public int[] getPegs() {
+		return getPegs(secretGuess, currentGuess);
+	}
+	
+	/**
+	 * returns the black and white pegs 
+	 * white peg - 1 - means there is one of the right color but in the wrong place
+	 * black peg - 2 - means there is one with the right placement and color
+	 * @param guessOne the guess that the second guess is compared to 
+	 * @param guessTwo the guess by the player that we want the pegs from 
+	 * @return an array of 4 ints that represent the small black and white pegs for the current guess
+	 */
+	public int[] getPegs(int[] guessOne, int[] guessTwo) {
+
+		int numBlack = 0;
+		int numWhite = 0;
+		int[] tempPegs = new int[numRows];
+
+		//go through each color and find matches
+		for(int i = 0; i < numColors; i++) {
+			int numInSecret = 0;
+			int numInGuess = 0;
+			int numBlackInGuess = 0;
+			//for each position check if there is one of the chosen color in guess or in secret
+			for(int j = 0; j < numRows; j++) {
+				if(guessOne[j] == i) {
+					numInSecret++;
+				}
+				if(guessTwo[j] == i) {
+					numInGuess++;
+				}
+				if(guessOne[j] == i && guessTwo[j] == i) {
+					numBlackInGuess++;
+					numBlack++;
+				}
+			}
+			
+			//if there is the same number of this color in both the secret and the guess,
+			//that number will be the sum of the black and white pegs for that color
+			if(numInSecret <= numInGuess) {
+				numWhite += numInSecret - numBlackInGuess;
+			} else {
+				numWhite += numInGuess - numBlackInGuess;
+			}
+		}
+		
+		Arrays.fill(tempPegs, 0);
+		//the first numBlack entries will be 2 
+		for(int i = 0; i < numBlack; i++) {
+			tempPegs[i] = 2;
+		}
+		
+		//afer those first entries the next numWhite entries will be 1 
+		for(int i = numBlack; i < numBlack + numWhite; i++) {
+			tempPegs[i] = 1;
+		}
+		
+		return tempPegs;
+	}
+	
+	/**
+	 * tells you whether you're a winner or not 
+	 * (you win if you still have turns left and your pegs are all twos)
+	 * @return true if you win, false otherwise 
+	 */
+	public boolean isWinner() {
+		boolean allTwos = true;
+		for(int i = 0; i < numRows; i++) {
+			if(currentPegs[i] != 2) {
+				allTwos = false;
+			}
+		}
+		return (numGuesses > 0 && allTwos);
+	}
+	
+	/**
+	 * creates an arraylist of all possible combination of pegs for a given number of rows and columns
+	 * recursive because why not 
+	 * @param numRows number of pegs in a row 
+	 * @param numColors number of possible colors
+	 * @return the arraylist of all possible combos
+	 */
+	public ArrayList<int[]> createCombos(int numRows, int numColors) {
+		ArrayList<int[]> tempList = new ArrayList<int[]>();
+		if (numRows == 0) {  // Just one combination, of size 0
+			tempList.add(new int[0]); // Weird, but I need an empty combo
+			return tempList;
+		}
+		else {  // length is greater than 1; use recursion
+			ArrayList<int[]> smallCombos = createCombos(numRows - 1,numColors);
+			for (int small = 0; small < smallCombos.size(); small++) {
+				for (int color = 0; color < numColors; color++) {
+					// Build new, larger combo
+					int[] tempCombo = new int[numRows];
+					
+					// copy small combo to new combo, and add color
+					for (int k = 0; k < numRows - 1; k++) {
+						tempCombo[k] = smallCombos.get(small)[k];
+					}
+					tempCombo[numRows - 1] = color;
+					
+					// Add to list of combos
+					tempList.add(tempCombo);
+				}
+
+			}
+			return tempList;	
+		}
+	}
+
+	/**
+	 * eliminates all solutions from possibleSolutions that don't match with the current guess and pegs
+	 */
+	public void calculateRemainingSolutions() {
+		int[] targetPegs = getPegs(secretGuess, currentGuess);
+		for(int i = possibleSolutions.size() - 1; i >= 0; i--) {
+			int[] solutionPegs = getPegs(possibleSolutions.get(i), currentGuess);
+			if(!Arrays.equals(targetPegs, solutionPegs)) {
+				possibleSolutions.remove(i);
+			}
+		}
+	}
+	
+	/**
+	 * returns the arraylist of possible solutions that have not been eliminated
+	 * @return the arraylist of possible solutions 
+	 */
+	public ArrayList<int[]> getPossibleSolutions() {
+		return possibleSolutions;
+	}
+	
+	/**
+	 * returns the number of possible solutions that have not been eliminated by calculateRemainingSolutions
+	 * @return the number of possible solutions
+	 */
+	public int getNumPossibleSolutions() {
+		calculateRemainingSolutions();
+		return possibleSolutions.size();
+	}
+	
+	/**
+	 * method to print out an int[] nicely
+	 * @param array the array to print 
+	 */
+	public void printArray(int [] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+	
 }
